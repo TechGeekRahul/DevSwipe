235c0e9f9662647e9af5ac25ee5ecd1954f61191
Rahul Kumar Singh <contactrahulkrsingh@gmail.com>
2025-03-29 19:05:44 +0530

    completed and tested various api endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,25 +2,95 @@ const express = require("express");
 const app =  express();
 const connectDb = require("./config/database")
 const User = require("./models/user")
+app.use(express.json())
 
 app.post("/signup",async (req,res)=>{
-const userObj = {
-    firstName: "Elon",
-    lastName: "Musk",
-    emailId:"elon@gmail.com",
-    password:"123852"
-}
-
-
-
 try{
-    const newUser = new User(userObj);
+    if(req.body.skills.length > 10){
+        throw new Error("You can't add more than 10 skills");
+    }
+    const newUser = new User(req.body);
     await newUser.save();
     res.send("User added successfully")
 }catch(err){
     res.status(400).send("error adding the user" + err.message);
 }
 })
+app.get("/user",async (req,res)=>{
+try {
+    const email = req.body.emailId;
+    const user = await User.find({emailId: email});
+    if(user.length != 0){
+        res.send(user)
+    }else{
+        res.status(404).send("user not found")
+    }
+    
+} catch (error) {
+    res.status(400).send("Something went wrong");
+}
+})
+
+app.get("/feed",async (req,res)=>{
+    try {
+    
+        const user = await User.find({});
+        
+            res.send(user)
+        
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+    })
+
+
+app.delete("/user",async(req,res)=>{
+    try{
+        const userId = req.body.userId;
+        const user =  await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+})
+
+
+
+app.patch("/user/:userId",async(req,res)=>{
+    try{
+        const userId = req.params?.userId;
+        const data = req.body
+        const allowedUpdates = ["firstName","lastName","skills","gender","photoUrl","about","userId"];
+        const isAllowedUpdates = Object.keys(data).every((k)=>
+            allowedUpdates.includes(k)
+        )
+        if(!isAllowedUpdates){
+            throw new Error("Update not allowed");
+        }
+        if(req.body.skills.length > 10){
+            throw new Error("You can't add more than 10 skills");
+        }
+        
+        
+        const user =  await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"before",
+            runValidators:true,
+        });
+        res.send("User updated successfully");
+    }catch(err){
+        res.status(400).send("Error updating the data " + err.message);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.use('/',(err,req,res,next)=>{
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,21 +1,65 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
-        required: true
+        required: true,
+        minLength:5,
+        maxLength:50,
     },
     lastName:{
-        type:String
+        type:String,
     },
     emailId:{
         type:String,
+        lowerCase:true,
+        trim:true,
         required: true,
-        unique: true
+        unique: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is not valid");
+            }
+        }
     },
     password:{
-        type:Number,
+        type:String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a strong password");
+            }
+        }
+    },
+    age:{
+        type:Number,
+        
+    },
+    gender:{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        }
+        
+    },
+    photoUrl:{
+        type:String,
+        default:"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg?semt=ais_hybrid",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("URL is not valid");
+            }
+        }
+    },
+    about:{
+        type:String,
+        default:"This is a default about of the user"
+    },
+    skills:{
+        type:[String]
     }
 })
 const userModal = mongoose.model("User",userSchema);
