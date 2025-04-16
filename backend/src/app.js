ecc52eee05c0ff5069a395064485aa57dcd4b459
Rahul Kumar Singh <contactrahulkrsingh@gmail.com>
2025-04-16 12:57:01 +0530

    fixed cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const userRouter = require("./routes/user")
 require("dotenv").config();
 
 app.use(cors({
-    origin: ["https://dev-swipe-rahul.vercel.app", "http://localhost:5173"],
+    origin: ["https://dev-swipe-rahul.vercel.app", "http://localhost:5173","http://16.171.33.120"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
